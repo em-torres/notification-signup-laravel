17d3e5544cb73e6da690522671f87e14c43a1a88
Edwin Marte Torres <kdgpro15@gmail.com>
2019-08-22 21:19:46 -0400

    220819003 - Implementando notificacion despues de validar el usuario

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use App\Notifications\UserCreatedToAdmin;
 use Illuminate\Http\Request;
+use Notification;
 
 class HomeController extends Controller
 {
@@ -24,5 +27,24 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function sendUserSignedUpNotification()
+    {
+        $user = User::first();
+
+        $details = [
+            'greeting' => 'Hi Admin!!',
+            'body' => ('A new user ' . $user->name . ' has signed up. Try to send greetings at ' . $user->email),
+            'thanks' => 'Thanks!!',
+            'actionText' => 'View My Site',
+            'actionURL' => url('/'),
+            'notif_if' => date("Y") . rand(1, 1000),
+            'admin_email' => 'edwin.sonic@gmail.com'
+        ];
+
+        Notification::send($user, new UserCreatedToAdmin($details));
+
+        return redirect('/home');
     }
 }
